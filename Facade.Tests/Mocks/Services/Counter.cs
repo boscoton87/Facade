a8ee471d34ed4fc07000632c943e4d0b764637ba
Poseidon\Alex <alex.l.harper@outlook.com>
2019-03-30 15:56:52 -0500

    Facade.Tests: Included Counter Service for testing Register functionality.

---
+++ Facade.Tests/Mocks/Services/Counter.cs
@@ -0,0 +1,31 @@
+﻿using Facade.Tests.Mocks.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Facade.Tests.Mocks.Services
+{
+    public class Counter : ICounter
+    {
+        private int Count { get; set; } = 0;
+
+        private string Name { get; set; } = "DefaultCounter";
+
+        public string GetStatus()
+        {
+            return $"{Name}: {Count}";
+        }
+
+        public void Increment()
+        {
+            Count++;
+        }
+
+        public Counter(string name)
+        {
+            Name = name;
+        }
+    }
+}
